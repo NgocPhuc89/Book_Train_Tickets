6117ffce711d4fa36631768eeaf391a170351218
NgocPhuc89 <ngocphuc966@gmail.com>
2023-10-30 11:27:56 +0700

    ip

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 
+const CUSTOMER_API = `https://json-server-vercel-ten-theta.vercel.app/Customer`;
+
 class CustomerService {
 
     static getCustomers() {
-        return axios.get(`http://localhost:3000/Customer`);
+        return axios.get(CUSTOMER_API);
     }
     static getCustomerSearch(search) {
-        return axios.get(`http://localhost:3000/Customer?${search ? `&q=${encodeURIComponent(search)}` : ''}`);
+        return axios.get(CUSTOMER_API + `?${search ? `&q=${encodeURIComponent(search)}` : ''}`);
     }
     static getCustomer(id) {
-        return axios.get(`http://localhost:3000/Customer/${id}`);
+        return axios.get(CUSTOMER_API + `/${id}`);
     }
     static postCustomer(data) {
-        return axios.post('http://localhost:3000/Customer', data);
+        return axios.post(CUSTOMER_API, data);
     }
     static editCustomer(id, data) {
-        return axios.patch(`http://localhost:3000/Customer/${id}`, data);
+        return axios.patch(CUSTOMER_API + `/${id}`, data);
     }
     static deleteCustomer(id) {
-        return axios.delete(`http://localhost:3000/Customer/${id}`)
+        return axios.delete(CUSTOMER_API + `/${id}`)
     }
 }
 export default CustomerService;
